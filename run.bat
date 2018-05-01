fabf0a2af6e81cb806a9b6883c83bd462cc5d28e
Robert <bielas.robert95@gmail.com>
2018-05-01 18:53:42 +0200

    init

--- run.bat
+++ run.bat
@@ -1,8 +1,8 @@
 md bin
 md lib
 dir /s /B *.java > sources
-call gradlew build
-call gradlew copyRuntimeLibs
+call gradle build
+call gradle copyRuntimeLibs
 javac -d bin -cp "lib/*;bin" @sources
 java -cp "bin;lib/*" ethtest.CustomTest 
 rem java -cp "out/production/classes;lib/*" ethtest.CustomTest
