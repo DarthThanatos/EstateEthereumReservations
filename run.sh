186226a876f9dc22952d8f6b80650d10380a2b98
Robb <robert.bielas@interia.pl>
2018-05-03 10:59:27 +0200

    script

--- run.sh
+++ run.sh
@@ -1,8 +1,9 @@
 mkdir bin
 mkdir lib
 find -name "*.java" > sources
-./gradlew build
-./gradlew copyRuntimeLibs
+export PATH=$PATH:$HOME/gradle/gradle-4.7/bin
+gradle build
+gradle copyRuntimeLibs
 javac -d bin -cp "lib/*:bin" @sources
-java -cp "bin;lib/*" main.Main
+java -cp "bin:lib/*" main.Main
 # java -cp "out/production/classes:lib/*" main.Main
