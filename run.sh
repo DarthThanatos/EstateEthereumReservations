b241b3bc68890c493129e663f15588bba25bfe07
Robert <bielas.robert95@gmail.com>
2018-05-10 23:01:19 +0200

    test

--- run.sh
+++ run.sh
@@ -6,5 +6,5 @@ echo $PATH
 gradle build
 gradle copyRuntimeLibs
 javac -d bin -cp "lib/*:bin" @sources
-java -cp "bin:lib/*" main.Main
+java -cp "bin:lib/*:src/main/resources" main.Main
 # java -cp "out/production/classes:lib/*" main.Main
